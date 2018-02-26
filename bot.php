1d79e6a2121fc1b259ca17f7417e034cdab8016e
pea61 <36658277+pea61@users.noreply.github.com>
2018-02-26 15:47:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,17 +10,27 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'สวัสดี') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['มีอะไรให้รับใช้ขอรับ'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			if ($event['type'] == 'message' && $event['message']['type'] == 'ปิดไฟ') {
+			// Get text sent
+			$text = $event['message']['ปิดไฟแล้วขอรับ'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+				if ($event['type'] == 'message' && $event['message']['type'] == 'เปิดไฟ') {
+			// Get text sent
+			$text = $event['message']['เปิดไฟแล้วขอรับ'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			//$messages = [
+				//'type' => 'text',
+				//'text' => $text
+			//];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
