9fd3070b1221331181f26c62d2cb58b1a2dcc6ea
pea61 <36658277+pea61@users.noreply.github.com>
2018-02-26 16:08:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,23 +8,23 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
-	foreach ($events['events'] as $event) {
+	foreach ($events['events' as $event) {
 		if(‘message’ == $event->type){
 // debug
 //file_put_contents(“message.json”, json_encode($event));
 $text = $event->message->text;
 
-if (preg_match(“/สวัสดี/”, $text)) {
-$text = “มีอะไรให้จ่าวิสรับใช้ครับ”;
+if (preg_match(“/hello/”, $text)) {
+$text = “hello sis”;
 }
 
-if (preg_match(“/เปิดทีวี/”, $text)) {     //หากในแชตที่ส่งมามีคำว่า เปิดทีวี ก็ให้ส่ง mqtt ไปแจ้ง server เราครับ
+if (preg_match(“/on/”, $text)) {     //หากในแชตที่ส่งมามีคำว่า เปิดทีวี ก็ให้ส่ง mqtt ไปแจ้ง server เราครับ
 
-$text = “เปิดทีวีให้แล้วคร้าบบบบ”;
+$text = “on led”;
 }
-if (preg_match(“/ปิดทีวี/”, $text) and !preg_match(“/เปิดทีวี/”, $text)) {
+if (preg_match(“/off/”, $text) and !preg_match(“/on/”, $text)) {
 
-$text = “จ่าปิดทีวีให้แล้วนะครับ!!”;
+$text = “off!!”;
 }
 $response = $bot->replyText($event->replyToken, $text); // ส่งคำ reply กลับไปยัง line application
 
