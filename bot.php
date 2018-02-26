1cab587624c42c69849f324caeadc5830a51b4bb
pea61 <36658277+pea61@users.noreply.github.com>
2018-02-26 16:17:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,48 +1,40 @@
-<?php
-$access_token = 'IrN10smd9lGZGp0JtOOoBJpAvSvDPFVNnDbTdxVbnU2Xv9YNaABrfKI2LxXxRH59XxerqJx3otWj0OqohFtMLiwSJy6fEEYarDN9KVKol7CqHo1GzqPST1DJI4hvg04yIDQiNwa2M1UD8K4SRn4XawdB04t89/1O/w1cDnyilFU=';
+[code] <?php
+require(“vendor/autoload.php”);
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events' as $event) {
-		if(‘message’ == $event->type){
+
+
+
+$token = 'IrN10smd9lGZGp0JtOOoBJpAvSvDPFVNnDbTdxVbnU2Xv9YNaABrfKI2LxXxRH59XxerqJx3otWj0OqohFtMLiwSJy6fEEYarDN9KVKol7CqHo1GzqPST1DJI4hvg04yIDQiNwa2M1UD8K4SRn4XawdB04t89/1O/w1cDnyilFU='; //นำ token ที่มาจาก line developer account ของเรามาใส่ครับ
+
+$httpClient = new CurlHTTPClient($token);
+$bot = new LINEBot($httpClient, [‘channelSecret’ => $token]);
+// webhook
+$jsonStr = file_get_contents(‘php://input’);
+$jsonObj = json_decode($jsonStr);
+print_r($jsonStr);
+foreach ($jsonObj->events as $event) {
+if(‘message’ == $event->type){
 // debug
 //file_put_contents(“message.json”, json_encode($event));
 $text = $event->message->text;
 
-if (preg_match(“/hello/”, $text)) {
-$text = “hello sis”;
+if (preg_match(“/สวัสดี/”, $text)) {
+$text = “มีอะไรให้จ่าวิสรับใช้ครับ”;
 }
 
-if (preg_match(“/on/”, $text)) {     //หากในแชตที่ส่งมามีคำว่า เปิดทีวี ก็ให้ส่ง mqtt ไปแจ้ง server เราครับ
-
-$text = “on led”;
+if (preg_match(“/เปิดทีวี/”, $text)) {   
+$text = “เปิดทีวีให้แล้วคร้าบบบบ”;
 }
-if (preg_match(“/off/”, $text) and !preg_match(“/on/”, $text)) {
+if (preg_match(“/ปิดทีวี/”, $text) and !preg_match(“/เปิดทีวี/”, $text)) {
 
-$text = “off!!”;
+$text = “จ่าปิดทีวีให้แล้วนะครับ!!”;
 }
 $response = $bot->replyText($event->replyToken, $text); // ส่งคำ reply กลับไปยัง line application
 
 }
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
 }
-echo "OK";
+
+?>
+[/code]
