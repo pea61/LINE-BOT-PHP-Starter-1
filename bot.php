291f4e7d342e878d81efb79e7b9a77da03a48f5c
pea61 <36658277+pea61@users.noreply.github.com>
2018-02-26 15:49:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,10 +27,10 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			//$messages = [
-				//'type' => 'text',
-				//'text' => $text
-			//];
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
