90e71dd08be3c58fd39c0e0ca515011ecc22a905
pea61 <36658277+pea61@users.noreply.github.com>
2018-03-20 10:38:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-	 
+
 	$strAccessToken = "IrN10smd9lGZGp0JtOOoBJpAvSvDPFVNnDbTdxVbnU2Xv9YNaABrfKI2LxXxRH59XxerqJx3otWj0OqohFtMLiwSJy6fEEYarDN9KVKol7CqHo1GzqPST1DJI4hvg04yIDQiNwa2M1UD8K4SRn4XawdB04t89/1O/w1cDnyilFU=";
 	 
 	$content = file_get_contents('php://input');
